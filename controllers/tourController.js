ed86341cec8af5b195a72024408590571f74cf67
Jayant <jayantkodam2223@gmail.com>
2022-05-30 22:53:44 +0530

    MongDB Mongoose Integration

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,117 +1,186 @@
-const fs = require('fs');
+const tourModel = require('../models/tourModel');
 
-const tourFileName = `${__dirname}/../dev-data/data/tours-simple.json`;
+//const tourFileName = `${__dirname}/../dev-data/data/tours-simple.json`;
 
-const tours = JSON.parse(fs.readFileSync(tourFileName));
+// const tours = JSON.parse(fs.readFileSync(tourFileName));
 
-exports.checkID = (req, res, next, val) => {
-  console.log(`Tour ID is ${val}`);
-  const id = req.params.id * 1;
-  if (id > tours.length) {
-    return res.status(404).json({
+// exports.checkID = (req, res, next, val) => {
+//   const id = req.params.id * 1;
+//   if (id > tours.length) {
+//     return res.status(404).json({
+//       status: 'Failure',
+//       message: 'Tour not found',
+//     });
+//   }
+//   next();
+// };
+
+// exports.checkBody = (req, res, next) => {
+//   if (!req.body.name || !req.body.price) {
+//     return res.status(400).json({
+//       status: 'Failure',
+//       message: 'Missing name or price',
+//     });
+//   }
+//   next();
+// };
+
+exports.getAllTours = async (req, res) => {
+  try {
+    const tours = await tourModel.find();
+
+    res.status(200).json({
+      status: 'success',
+      result: tours.length,
+      data: {
+        tours: tours,
+      },
+    });
+  } catch (error) {
+    res.status(400).json({
       status: 'Failure',
-      message: 'Tour not found',
+      message: error,
     });
   }
-  next();
 };
 
-exports.checkBody = (req, res, next) => {
-    if (!req.body.name || !req.body.price) {
-        return res.status(400).json({
-            status: 'Failure',
-            message: 'Missing name or price',
-        });
-    }   
-    next();
-}
+exports.createTour = async (req, res) => {
+  try {
+    const newTour = await tourModel.create(req.body);
 
-exports.getAllTours = (req, res) => {
-  res.status(200).json({
-    status: 'success',
-    result: tours.length,
-    data: {
-      tours,
-    },
-  });
-};
-
-exports.createTour = (req, res) => {
-  const newTour = Object.assign({ id: tours.length + 1 }, req.body);
-  tours.push(newTour);
-  fs.writeFile(tourFileName, JSON.stringify(tours), (err) => {
-    if (err) {
-      res.status(500).json({
-        status: 'Failure',
-        message: err.message,
-      });
-    }
     res.status(201).json({
       status: 'Success',
       result: 'User created',
       data: {
-        newTour,
+        tour: newTour,
       },
     });
-    console.log('File written successfully');
-  });
+  } catch (err) {
+    res.status(400).json({
+      status: 'Failure',
+      message: err,
+    });
+  }
+
+  // const newTour = Object.assign({ id: tours.length + 1 }, ...req.body);
+  // tours.push(newTour);
+  // fs.writeFile(tourFileName, JSON.stringify(tours), (err) => {
+  //   if (err) {
+  //     res.status(500).json({
+  //       status: 'Failure',
+  //       message: err.message,
+  //     });
+  //   }
+  //   res.status(201).json({
+  //     status: 'Success',
+  //     result: 'User created',
+  //     data: {
+  //       newTour,
+  //     },
+  //   });
+  // });
 };
 
-exports.getTourById = (req, res) => {
-  const id = req.params.id * 1;
-  const tour = tours.find((el) => el.id === id);
-  res.status(200).json({
-    status: 'Success',
-    data: {
-      tour,
-    },
-  });
-};
+exports.getTourById = async (req, res) => {
+  try {
+    const tour = await tourModel.findById(req.params.id);
 
-exports.updateTourById = (req, res) => {
-  const id = req.params.id * 1;
-
-  const tour = tours.find((el) => el.id == id);
-  
-  const updated_tour = Object.assign({ id: tour.id }, req.body);
-  const index = tours.indexOf(tour);
-  tours[index] = updated_tour;
-  fs.writeFile(tourFileName, JSON.stringify(tours), (err) => {
-    if (err) {
-      res.status(500).json({
-        status: 'Failure',
-        message: err.message,
-      });
-    }
-    res.status(201).json({
+    res.status(200).json({
       status: 'Success',
-      result: 'User Updated',
+      result: 'Tour found',
       data: {
-        updated_tour,
+        tour,
       },
     });
-    console.log('File written successfully');
-  });
+  } catch (error) {
+    res.status(400).json({
+      status: 'Failure',
+      message: error,
+    });
+  }
+  // const tour = tours.find((el) => el.id === id);
+  // res.status(200).json({
+  //   status: 'Success',
+  //   data: {
+  //     tour,
+  //   },
+  // });
 };
 
-exports.deleteTourById = (req, res) => {
-  const id = req.params.id * 1;
-  const tour = tours.find((el) => el.id == id);
-  
-  const index = tours.indexOf(tour);
-  tours.splice(index, 1);
-  fs.writeFile(tourFileName, JSON.stringify(tours), (err) => {
-    if (err) {
-      res.status(500).json({
-        status: 'Failure',
-        message: err.message,
-      });
-    }
-    res.status(201).json({
-      status: 'Success',
-      result: 'User Deleted',
-      data: null,
+exports.updateTourById = async (req, res) => {
+  try {
+    const updatedTour = await tourModel.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
     });
-    console.log('File written successfully');
-  });
+    res.status(200).json({
+      status: 'Success',
+      result: 'Tour updated',
+      data: {
+        tour: updatedTour,
+      },
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: 'Failure',
+      message: error,
+    });
+  }
+
+  // const id = req.params.id * 1;
+  // const tour = tours.find((el) => el.id === id);
+  // const updatedTour = Object.assign({ id: tour.id }, ...req.body);
+  // const index = tours.indexOf(tour);
+  // tours[index] = updatedTour;
+  // fs.writeFile(tourFileName, JSON.stringify(tours), (err) => {
+  //   if (err) {
+  //     res.status(500).json({
+  //       status: 'Failure',
+  //       message: err.message,
+  //     });
+  //   }
+  //   res.status(201).json({
+  //     status: 'Success',
+  //     result: 'User Updated',
+  //     data: {
+  //       updatedTour,
+  //     },
+  //   });
+  // });
+};
+
+exports.deleteTourById = async (req, res) => {
+  try {
+    const tour = await tourModel.findByIdAndDelete(req.params.id);
+
+    res.status(200).json({
+      status: 'Success',
+      result: 'Tour deleted',
+      data: {
+        tour,
+      },
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: 'Failure',
+      message: error,
+    });
+  }
+  // const id = req.params.id * 1;
+  // const tour = tours.find((el) => el.id === id);
+  // const index = tours.indexOf(tour);
+  // tours.splice(index, 1);
+  // fs.writeFile(tourFileName, JSON.stringify(tours), (err) => {
+  //   if (err) {
+  //     res.status(500).json({
+  //       status: 'Failure',
+  //       message: err.message,
+  //     });
+  //   }
+  //   res.status(201).json({
+  //     status: 'Success',
+  //     result: 'User Deleted',
+  //     data: null,
+  //   });
+  // });
 };
---
+++ models/tourModel.js
@@ -0,0 +1,62 @@
+const mongoose = require('mongoose');
+
+const toursSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+    unique: true,
+    trim: true,
+    maxlength: [40, 'A tour name must have less or equal then 40 characters'],
+    minlength: [10, 'A tour name must have more or equal then 10 characters'],
+  },
+  duration: {
+    type: Number,
+    required: [true, 'A tour must have a duration'],
+  },
+  maxGroupSize: {
+    type: Number,
+    required: [true, 'A tour must have a group size'],
+  },
+  difficulty: {
+    type: String,
+    required: [true, 'A tour must have a difficulty'],
+    enum: {
+      values: ['easy', 'medium', 'difficult'],
+      message: 'Difficulty is either: easy, medium or difficult',
+    },
+  },
+  ratingAverage: {
+    type: Number,
+    default: 4.5,
+  },
+  ratingsQuantity: {
+    type: Number,
+    default: 0,
+  },
+  priceDiscount: Number,
+  summary: {
+    type: String,
+    trim: true,
+    required: [true, 'A tour must have a summary'],
+  },
+  description: {
+    type: String,
+    trim: true,
+    required: [true, 'A tour must have a description'],
+  },
+  imageCover: {
+    type: String,
+    required: [true, 'A tour must have a cover image'],
+  },
+  images: [String],
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+    select: false,
+  },
+  startDates: [Date],
+});
+
+const Tour = mongoose.model('Tour', toursSchema);
+
+module.exports = Tour;
