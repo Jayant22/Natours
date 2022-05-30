69d7ad99874212c9fb93466c25c666965bd11b72
Jayant <jayantkodam2223@gmail.com>
2022-05-30 19:27:04 +0530

    MongoDB Setup

--- server.js
+++ server.js
@@ -1,6 +1,17 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
 dotenv.config({ path: './config.env' });
+
+const DB = process.env.DATABASE_NAME.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('DB connection successful!'))
+  .catch((err) => console.warn(err));
 
 const app = require('./app');
 
