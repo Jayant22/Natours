f9a33c099ae8a2e0b267e96cea6b8c6c5427d765
Jayant <jayantkodam2223@gmail.com>
2022-07-07 20:28:19 +0530

    User Creation & Authentication

---
+++ models/userModel.js
@@ -0,0 +1,68 @@
+//const crypto = require('crypto');
+const mongoose = require('mongoose');
+const validator = require('validator');
+const bcrypt = require('bcryptjs');
+
+const dotenv = require('dotenv');
+
+dotenv.config({ path: '../config.env' });
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A user must have a name'],
+  },
+  email: {
+    type: String,
+    required: [true, 'A user must have an email'],
+    unique: true,
+    lowercase: true,
+    validate: [validator.isEmail, 'Please provide a valid email'],
+  },
+  photo: {
+    type: String,
+    default: 'https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50?s=200',
+  },
+  password: {
+    type: String,
+    required: [true, 'A user must have a password'],
+    minlength: [8, 'A password must be at least 8 characters'],
+    select: false,
+  },
+  passwordConfirm: {
+    type: String,
+    required: [true, 'A user must have a password'],
+    validate: {
+      validator: function (el) {
+        return el === this.password;
+      },
+      message: 'Passwords do not match',
+    },
+  },
+  active: {
+    type: Boolean,
+    default: true,
+    select: false,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+    select: false,
+  },
+});
+
+userSchema.pre('save', async function (next) {
+  // Only run this function if password was actually modified
+  if (!this.isModified('password')) return next();
+
+  // Hash the password with cost of 12
+  this.password = await bcrypt.hash(this.password, 12);
+
+  // Delete passwordConfirm field
+  this.passwordConfirm = undefined;
+  next();
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
